f6252be9dd131bd63fed190ae98713cd26327020
Adam Anderson <adamandersonis@gmail.com>
2016-11-25 16:00:14 -0500

    Upgrade to Rails 5

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-CountrySelectTest::Application.config.session_store :cookie_store, key: '_country_select_test_session'
+Rails.application.config.session_store :cookie_store, key: '_foo_session'
