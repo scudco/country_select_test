062cf6d4bd46f0238fb3ae5eb99a1fb5ce282b30
Adam Anderson <3806+scudco@users.noreply.github.com>
2018-07-01 16:37:41 -0400

    Upgrade to Rails 5.2

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,16 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Foo
+module CountrySelectTest
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
     I18n.config.available_locales = [:en,:es]
   end
 end
